187c4cd44859284ed7ffe183fdb3fe81c596e32c
杨文枝 <313097973@qq.com>
2024-05-29 10:13:30 +0800

    fix: format code

--- src/Handler/ThrottleHandler.php
+++ src/Handler/ThrottleHandler.php
@@ -70,12 +70,11 @@ class ThrottleHandler
      * @throws ContainerExceptionInterface
      */
     public function execute(
-        int   $limit = 60,
-        int   $timer = 60,
+        int $limit = 60,
+        int $timer = 60,
         mixed $key = null,
         mixed $callback = null
-    ): void
-    {
+    ): void {
         // 获取Key
         $key = $this->getKey($key);
         // 限制频次
@@ -105,7 +104,7 @@ class ThrottleHandler
     public function getSignature(mixed $key): string
     {
         if (is_array($key)) {
-            return (string)call_user_func($key);
+            return (string) call_user_func($key);
         }
         return sha1($key . '_' . $this->getRealIp());
     }
@@ -113,7 +112,7 @@ class ThrottleHandler
     public function getRealIp(mixed $request = null): string
     {
         $request = $request ?? Context::get(RequestInterface::class);
-        if (!$request) {
+        if (! $request) {
             throw new RuntimeException('No request context');
         }
         /** @var RequestInterface $request */
@@ -137,10 +136,10 @@ class ThrottleHandler
      */
     public function frequency(string $key, int $timer): int
     {
-        if (!$this->redis->get($key)) {
+        if (! $this->redis->get($key)) {
             $this->redis->setex($key, $timer, 0);
         }
-        return (int)$this->redis->incr($key);
+        return (int) $this->redis->incr($key);
     }
 
     public function getTimer($timer)
@@ -193,7 +192,7 @@ class ThrottleHandler
             'X-RateLimit-Remaining' => $remain,  // 在指定时间段内剩下的请求次数
         ];
 
-        if (!is_null($retry)) {  // 只有当用户访问频次超过了最大频次之后才会返回以下两个返回头字段
+        if (! is_null($retry)) {  // 只有当用户访问频次超过了最大频次之后才会返回以下两个返回头字段
             $headers['X-RateLimit-Retry'] = $retry;  // 距离下次重试请求需要等待的时间（s）
             $headers['X-RateLimit-Reset'] = Carbon::now()->addSeconds($retry)->getTimestamp();  // 距离下次重试请求需要等待的时间戳（s）
         }
@@ -260,7 +259,7 @@ class ThrottleHandler
      */
     public function getTimeUntilNextRetry(string $key): int
     {
-        $timer = (int)$this->redis->get($this->getKeyTimer($key));
+        $timer = (int) $this->redis->get($this->getKeyTimer($key));
         return max(0, $timer - Carbon::now()->getTimestamp());
     }
 
